fdc3cd67489e0f1d68880ea8bd80c71e2fa7ee97
Your Name <altynnikov313@gmail.com>
2022-05-31 12:50:03 +0300

    test

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-let londonCity = document.querySelector(".london"),
-  parisCity = document.querySelector(".paris"),
-  newYorkCity = document.querySelector(".new-york"),
-  kharkivCity = document.querySelector(".kharkiv");
-
 function weatherCity() {
   let city = document.querySelector(".i-1").value;
 
@@ -32,6 +27,7 @@ function weatherCity() {
 }
 
 document.querySelector(".b-1").onclick = weatherCity;
+
 document.querySelector(".Kyiv").onclick = () => {
   document.querySelector(".pricing-table").style.display = "flex";
   fetch(
@@ -129,27 +125,27 @@ document.querySelector(".Paris").onclick = () => {
       // catch any errors
     });
 };
-// document.querySelector(".Kharkiv").onclick = () => {
-//   document.querySelector(".pricing-table").style.display = "flex";
-//   fetch(
-//     "http://api.openweathermap.org/data/2.5/weather?id=706483&appid=70e1ed322b02acbc57d443dd91065f3e"
-//   )
-//     .then(function (resp) {
-//       return resp.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//       document.querySelector(".package-name").textContent = data.name;
-//       document.querySelector(".price").innerHTML =
-//         Math.round(data.main.temp - 273) + "&deg;";
-//       document.querySelector(".disclaimer").textContent =
-//         data.weather[0]["description"];
-//       //https://openweathermap.org/img/wn/02d@1x.png
-//       document.querySelector(
-//         ".features li"
-//       ).innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]["icon"]}@2x.png">`;
-//     })
-//     .catch(function () {
-//       // catch any errors
-//     });
-// };
+document.querySelector(".Kharkiv").onclick = () => {
+  document.querySelector(".pricing-table").style.display = "flex";
+  fetch(
+    "http://api.openweathermap.org/data/2.5/weather?id=706483&appid=70e1ed322b02acbc57d443dd91065f3e"
+  )
+    .then(function (resp) {
+      return resp.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      document.querySelector(".package-name").textContent = data.name;
+      document.querySelector(".price").innerHTML =
+        Math.round(data.main.temp - 273) + "&deg;";
+      document.querySelector(".disclaimer").textContent =
+        data.weather[0]["description"];
+      //https://openweathermap.org/img/wn/02d@1x.png
+      document.querySelector(
+        ".features li"
+      ).innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]["icon"]}@2x.png">`;
+    })
+    .catch(function () {
+      // catch any errors
+    });
+};
